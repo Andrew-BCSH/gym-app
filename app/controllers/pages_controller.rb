19709714573920aad0ac525c35358984300f011f
Andrew Brown <andrew.bcsh@outlook.com>
2023-10-11 13:39:28 +0800

    mejiro-gym-app

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def monthly_membership_payment
+  end
+
+  def book_a_private_class
+  end
+
+  def mejiro_coin
+  end
+
+  def payment_scanner
+  end
+
+  def weekly_class_schedule
+  end
+
+  def events
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home' # Define your root route
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Define routes for other actions
+  get '/monthly_membership_payment', to: 'pages#monthly_membership_payment'
+  get '/book_a_private_class', to: 'pages#book_a_private_class'
+  get '/mejiro_coin', to: 'pages#mejiro_coin'
+  get '/payment_scanner', to: 'pages#payment_scanner'
+  get '/weekly_class_schedule', to: 'pages#weekly_class_schedule'
+  get '/events', to: 'pages#events'
+
+  # Additional routes for other actions if needed
 end
