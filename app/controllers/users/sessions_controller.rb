ee51a47a2e99b0e4779d2f0ea41d94d96d4d6217
Andrew Brown <andrew.bcsh@outlook.com>
2023-11-02 11:29:59 +0800

    devise

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,12 +2,10 @@
 
 class Users::SessionsController < Devise::SessionsController
   # Your custom actions and methods can be defined here
-  # For example, you can add custom logic after a user signs in.
 
   # This will execute after a successful sign-in
   def after_sign_in_path_for(resource)
-    # You can customize where the user is redirected after signing in
-    # For example, redirect to the user's dashboard:
-    user_dashboard_path
+    # Redirect to the home page
+    '/home'
   end
 end
