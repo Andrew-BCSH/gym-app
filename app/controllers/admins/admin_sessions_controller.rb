7b270f44ab18f1c6574d0f74c5508c78dec48622
Andrew Brown <andrew.bcsh@outlook.com>
2023-12-08 12:12:41 +0800

    admin sessions

--- app/controllers/admins/admin_sessions_controller.rb
+++ app/controllers/admins/admin_sessions_controller.rb
@@ -1,4 +1,4 @@
-class Admins::SessionsController < Devise::SessionsController
+class Admins::AdminSessionsController < Devise::SessionsController
   # This will execute after a successful sign-in
   def after_sign_in_path_for(resource)
     # Redirect to the home page after sign-in
