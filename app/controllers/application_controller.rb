c7e53ad7d28e658ecca3b34060c31e3c5c4e8d12
Andrew Brown <andrew.bcsh@outlook.com>
2023-12-18 13:58:58 +0800

    include ext

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email])
   end
 
-
-  force_ssl if: :ssl_configured?
-
-  def ssl_configured?
-    !Rails.env.development?
-  end
 end
