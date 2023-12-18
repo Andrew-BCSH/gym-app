3b1c5150cad9c14711b2c0aec99117284015735e
Andrew Brown <andrew.bcsh@outlook.com>
2023-12-18 13:48:46 +0800

    include ext

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,36 @@ import "@hotwired/turbo-rails"
 import "controllers"
 // back button
 
-import "./qr_code_scanner.js"
+import QrScanner from 'qr-scanner';
+
+if (window.location.pathname === '/qr_code_scanner') {
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment", width: 720, height: 720 }, audio: false }) // Request the rear camera and microphone
+    .then((stream) => {
+
+      console.log({ stream });
+      const videoElement = document.querySelector('#camera1');
+      videoElement.srcObject = stream;
+      videoElement.play();
+
+      console.log(videoElement);
+
+      const qrScanner = new QrScanner(
+        videoElement,
+        (result) => {
+          window.location.href = result.data;
+        },
+        {
+          highlightScanRegion: true,
+          highlightCodeOutline: true,
+          returnDetailedScanResult: true,
+        }
+      );
+
+      qrScanner.start();
+    })
+    .catch((error) => {
+      console.error('Error! accessing rear camera and microphone:', error);
+    });
+
+}
