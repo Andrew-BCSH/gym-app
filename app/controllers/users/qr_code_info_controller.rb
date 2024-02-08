57808384ecb48cf80ed3f059709c801c850f8639
Andrew Brown <andrew.bcsh@outlook.com>
2024-02-08 15:11:17 +0800

    info

--- app/controllers/users/qr_code_info_controller.rb
+++ app/controllers/users/qr_code_info_controller.rb
@@ -1,12 +1,15 @@
 module Users
   class QrCodeInfoController < ApplicationController
-    # Retrieve the information stored in the QR code
-    # You can access the information based on the parameters passed in the request
-    @user_id = params[:user_id]
-    @mejiro_coin_balance = params[:mejiro_coin_balance]
-    # Add more information as needed
+    # Define an action method to handle requests
+    def show
+      # Retrieve the information stored in the QR code
+      # You can access the information based on the parameters passed in the request
+      @user_id = params[:user_id]
+      @mejiro_coin_balance = params[:mejiro_coin_balance]
+      # Add more information as needed
 
-    # Render the view template
-    # You can pass the retrieved information to the view template
+      # Render the view template
+      # You can pass the retrieved information to the view template
+    end
   end
 end
