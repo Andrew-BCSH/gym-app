90ac2dd279965bb420d21179e75f66a03b32009d
Andrew Brown <andrew.bcsh@outlook.com>
2024-02-20 11:50:17 +0800

    membership days

--- app/controllers/admins/memberships_controller.rb
+++ app/controllers/admins/memberships_controller.rb
@@ -1,21 +1,40 @@
-# app/controllers/admin/memberships_controller.rb
 class Admins::MembershipsController < AdminController
-
   def show
     # Retrieve all users and their memberships
     @users_with_memberships = User.includes(:memberships).all
   end
 
   def add_days
-    membership = Membership.find(params[:membership_id])
-    days_to_add = params[:days_to_add].to_i
+    user = User.find_by(id: params[:user_id]) || User.find_by(username: params[:user_id])
 
-    # Add logic to update membership days here
-    membership.update(days_of_membership: membership.days_of_membership + days_to_add)
-
-    respond_to do |format|
-      format.json { render json: { id: membership.id, days_of_membership: membership.days_of_membership } }
+    if user.nil?
+      flash[:error] = "User not found."
+      redirect_back(fallback_location: root_path)
+      return
     end
+
+    # Find or create membership for the user
+    membership = user.memberships.first_or_create
+
+    # Handle nil days_of_membership
+    membership.update(days_of_membership: 0) if membership.days_of_membership.nil?
+
+    days_to_modify = params[:days].to_i
+
+    # Check if the user wants to add or subtract days
+    if params[:action_type] == "add"
+      new_days = membership.days_of_membership + days_to_modify
+      membership.update(days_of_membership: new_days)
+      flash[:success] = "#{days_to_modify} days added successfully."
+    elsif params[:action_type] == "subtract"
+      new_days = [0, membership.days_of_membership - days_to_modify].max
+      membership.update(days_of_membership: new_days)
+      flash[:success] = "#{days_to_modify} days subtracted successfully."
+    else
+      flash[:error] = "Invalid action type."
+    end
+
+    redirect_back(fallback_location: root_path)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_14_075757) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_20_024658) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -122,6 +122,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_14_075757) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.integer "membership_days_remaining", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
