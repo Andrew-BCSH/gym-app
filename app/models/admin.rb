3b5a66eb4a3bf0a2940bdcbdb1f1abaa62695e35
Andrew Brown <andrew.bcsh@outlook.com>
2024-04-16 11:48:42 +0800

    remove column

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,7 +3,7 @@ class Admin < ApplicationRecord
   validates :admin_name, presence: true
   # Add any other validations or associations here
 
-  has_one :weekly_class_schedule
+ # has_one :weekly_class_schedule
 
   # Include Devise modules and custom attributes
   devise :database_authenticatable, :registerable,
