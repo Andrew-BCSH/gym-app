a5007073bf64edbf352557e6b7f00e965cf7d6a8
Andrew Brown <andrew.bcsh@outlook.com>
2024-04-16 18:08:04 +0800

    qr code info

--- app/controllers/users/qr_codes_controller.rb
+++ app/controllers/users/qr_codes_controller.rb
@@ -1,4 +1,6 @@
 class Users::QrCodesController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     # Logic to retrieve QR code data for the current user
     @qr_code_data = current_user.qr_code_data
