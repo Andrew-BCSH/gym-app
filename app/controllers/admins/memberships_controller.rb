9a4da4bb3f96d860fbd31c148bd4ec709e8dfebf
Andrew Brown <andrew.bcsh@outlook.com>
2024-05-02 19:56:23 +0800

    user days remaining

--- app/controllers/admins/memberships_controller.rb
+++ app/controllers/admins/memberships_controller.rb
@@ -5,7 +5,7 @@ class Admins::MembershipsController < AdminController
 
   def show
     # Retrieve all users and their memberships
-    @users_with_memberships = User.all.order(sort_column + ' ' + sort_direction)
+    @users_with_memberships = User.all.order(sort_column + ' ' + sort_direction).paginate(page: params[:page], per_page: 10)
   end
 
   # Controller action
