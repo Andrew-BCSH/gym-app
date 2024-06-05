c7a0287b5a4dfce4d27d884310333f6b5c6ba04b
Andrew Brown <andrew.bcsh@outlook.com>
2024-06-05 19:29:38 +0800

    events

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,24 +1,10 @@
 class MembershipsController < ApplicationController
   def index
-    @memberships = Membership.all
+    @memberships = Membership.includes(:user)
   end
 
   def show
     @membership = Membership.find(params[:id])
   end
 
-  def assign
-    user = User.find(params[:user_id])
-    membership = Membership.find(params[:membership_id])
-
-    if user && membership
-      user.memberships << membership
-      membership.update(user: user)
-      flash[:success] = "Membership assigned successfully."
-    else
-      flash[:error] = "User or Membership not found."
-    end
-
-    redirect_to some_path # Replace with the path you want to redirect to
-  end
 end
