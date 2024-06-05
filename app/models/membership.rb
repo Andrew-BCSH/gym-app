3d18d52d72f771b52db311dcccca70ec388deb6a
Andrew Brown <andrew.bcsh@outlook.com>
2024-06-05 19:21:26 +0800

    events

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,15 +1,12 @@
+
+
 class Membership < ApplicationRecord
   belongs_to :category
   belongs_to :user, optional: true
   monetize :price_cents
   has_many :orders, dependent: :destroy
 
-  # Add the new attribute
   validates :days_of_membership, presence: true, numericality: { only_integer: true, greater_than: 0 }
-
-  # Add attributes for name and type
   validates :name, presence: true
   validates :category, presence: true
-
-  # Other model code...
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,68 +1,43 @@
-# app/models/user.rb
 class User < ApplicationRecord
-  # Include default devise modules.
-  # Others available are:
+  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
   has_one :credit, dependent: :destroy
   has_many :membership_logs
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :top_ups, dependent: :destroy
 
-  belongs_to :membership, optional: true
-
-
   after_create :initialize_user_credit
-  # after_create :set_last_membership_start_date
 
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
 
   def initialize_user_credit
-    # Create a Credit record for the user with an initial balance of 0 Mejiro Coins
     Credit.create(user: self, balance: 0)
   end
 
-  # def set_last_membership_start_date
-  #   update(last_membership_start_date: Date.today)
-  # end
-
-  # Method to update user's balance
   def top_up_balance(amount_in_currency)
-    credit = self.credit
     if credit
-      # Update the balance by adding the top-up amount
       credit.update(balance: credit.balance + amount_in_currency)
-    else
-      # Handle the case where there's no credit record for the user
-      # You can create one or handle it as per your application logic
     end
   end
 
   def membership_days_remaining
-    total_remaining_days = 0
     if last_membership_start_date
-      total_remaining_days = [0, (last_membership_start_date + membership_days - Date.today).to_i].max
+      [0, (last_membership_start_date + membership_days - Date.today).to_i].max
+    else
+      0
     end
-    total_remaining_days
   end
-
-  # def last_membership_start_date
-  #   # Logic to fetch the last membership start date
-  # end
 
   def membership_name
     memberships.last&.name
   end
 
   def qr_code_data
-    # Define the data you want to include in the QR code for the user
     {
       user_id: id,
       mejiro_coin_balance: credit&.balance || 0,
-      # Add more data as needed
     }
   end
-
-  # Other user model code...
-
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  validates :username, presence: true, uniqueness: true
 end
