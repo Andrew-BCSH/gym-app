4923e19e2ee71c77ff4512f8ec8f35a13c82ca76
Andrew Brown <andrew.bcsh@outlook.com>
2024-06-06 20:13:51 +0800

    memberships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
-# app/models/user.rb
+
+
 class User < ApplicationRecord
-  # Include default devise modules.
-  # Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
   has_one :credit, dependent: :destroy
   has_many :membership_logs
   has_many :memberships
@@ -10,29 +8,18 @@ class User < ApplicationRecord
 
   belongs_to :membership, optional: true
 
-
   after_create :initialize_user_credit
-  # after_create :set_last_membership_start_date
-
 
   def initialize_user_credit
-    # Create a Credit record for the user with an initial balance of 0 Mejiro Coins
     Credit.create(user: self, balance: 0)
   end
 
-  # def set_last_membership_start_date
-  #   update(last_membership_start_date: Date.today)
-  # end
-
-  # Method to update user's balance
   def top_up_balance(amount_in_currency)
     credit = self.credit
     if credit
-      # Update the balance by adding the top-up amount
       credit.update(balance: credit.balance + amount_in_currency)
     else
       # Handle the case where there's no credit record for the user
-      # You can create one or handle it as per your application logic
     end
   end
 
@@ -44,24 +31,17 @@ class User < ApplicationRecord
     total_remaining_days
   end
 
-  # def last_membership_start_date
-  #   # Logic to fetch the last membership start date
-  # end
-
   def membership_name
     memberships.last&.name
   end
 
   def qr_code_data
-    # Define the data you want to include in the QR code for the user
     {
       user_id: id,
       mejiro_coin_balance: credit&.balance || 0,
-      # Add more data as needed
+      membership_name: membership_name # Include the membership name
     }
   end
-
-  # Other user model code...
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: true
