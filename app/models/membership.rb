e0e45c80f726763429c785183eabf532b1fd7906
Andrew Brown <andrew.bcsh@outlook.com>
2024-06-06 20:50:12 +0800

    memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,7 @@ class Membership < ApplicationRecord
   belongs_to :category
   belongs_to :user, optional: true
   monetize :price_cents
-  has_many :orders, dependent: :destroy
+  #has_many :orders, dependent: :destroy
 
   validates :days_of_membership, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :name, presence: true
