8288343e574c633e6ed072037ebec7281b7f06c3
Andrew Brown <andrew.bcsh@outlook.com>
2024-06-07 14:50:19 +0800

    memberships

--- app/controllers/admins/memberships_controller.rb
+++ app/controllers/admins/memberships_controller.rb
@@ -68,58 +68,60 @@ class Admins::MembershipsController < AdminController
       membership = Membership.find_by(id: params[:membership_id])
 
       if user.nil?
-        # You can optionally set a flash message here if needed
-        # flash[:error] = "User not found."
-      else
-        days_remaining = user.membership_days || 0
-        days_to_modify = 0
-
-        if membership.sku == "daily"
-          days_to_modify += params[:order_amount].to_i
-        elsif membership.sku == "monthly"
-          days_to_modify += params[:order_amount].to_i * 30
-        elsif membership.sku == "yearly"
-          days_to_modify += params[:order_amount].to_i * 365
-        end
-
-        if params[:action_type] == "add"
-          new_days = days_remaining + days_to_modify
-        elsif params[:action_type] == "subtract"
-          new_days = [0, days_remaining - days_to_modify].max
-        else
-          # You can optionally set a flash message here if needed
-          # flash[:error] = "Invalid action type."
-          redirect_to admins_memberships_show_path and return
-        end
-
-        membership_log = user.membership_logs.create
-        membership_log.membership_id = params[:membership_id]
-        membership_log.amount = params[:order_amount].to_i
-        membership_log.total_cost = params[:total_cost].to_i
-        membership_log.operation = params[:action_type]
-        if user.membership_days_remaining ==0
-          user.last_membership_start_date =DateTime.now
-        end
-
-        user.membership_days = new_days
-
-
-        if user.save()
-          membership_log.save!
-          # You can optionally set a flash message here if needed
-           flash[:success] = "#{days_to_modify} days #{params[:action_type]}ed successfully."
-        else
-          # You can optionally set a flash message here if needed
-           flash[:error] = "Failed to #{params[:action_type]} days."
-        end
+        flash[:error] = "User not found."
+        redirect_to admins_memberships_show_path and return
       end
 
-      # respond_to do |format|
-      #   format.turbo_stream
-      # end
+      if membership.nil?
+        flash[:error] = "Membership not found."
+        redirect_to admins_memberships_show_path and return
+      end
+
+      days_remaining = user.membership_days || 0
+      days_to_modify = 0
+
+      case membership.sku
+      when "daily"
+        days_to_modify += params[:order_amount].to_i
+      when "monthly"
+        days_to_modify += params[:order_amount].to_i * 30
+      when "yearly"
+        days_to_modify += params[:order_amount].to_i * 365
+      end
+
+      new_days = if params[:action_type] == "add"
+                   days_remaining + days_to_modify
+                 elsif params[:action_type] == "subtract"
+                   [0, days_remaining - days_to_modify].max
+                 else
+                   flash[:error] = "Invalid action type."
+                   redirect_to admins_memberships_show_path and return
+                 end
+
+      membership_log = user.membership_logs.create(
+        membership_id: params[:membership_id],
+        amount: params[:order_amount].to_i,
+        total_cost: params[:total_cost].to_i,
+        operation: params[:action_type]
+      )
+
+      user.last_membership_start_date = DateTime.now if user.membership_days_remaining == 0
+      user.membership_days = new_days
+
+      if user.memberships.exclude?(membership)
+        user.memberships << membership
+      end
+
+      if user.save
+        membership_log.save!
+        flash[:success] = "#{days_to_modify} days #{params[:action_type]}ed successfully."
+      else
+        flash[:error] = "Failed to #{params[:action_type]} days."
+      end
 
       redirect_to admins_show_membership_path(user_id: user.id)
     end
+
 
 
 
