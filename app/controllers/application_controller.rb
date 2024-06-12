99851826e0532189813dbbd83683687d7053f257
Andrew Brown <andrew.bcsh@outlook.com>
2024-06-12 14:12:52 +0800

    whatsapp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :photo, :whatsapp_number])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :photo, :password, :password_confirmation, :current_password, :whatsapp_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :whatsapp_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password, :whatsapp_number])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   end
 end
